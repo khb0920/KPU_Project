619aff751ab86bef7787eaf6398cceda6566bbff
kimhongbae <smr1995@naver.com>
2021-07-23 12:45:37 +0900

    backendserver

--- models/User.js
+++ models/User.js
@@ -6,25 +6,42 @@ const UserStorage = require("./UserStorage");
 class User  {
     constructor(body){
         this.body =body;
+        
     }
 
     async login() {
         
-        const { id, psword} = await UserStorage.getUserInfo(this.body.id);
+        try {
+        const kakao_account = await UserStorage.getUserInfo(this.body.kakao_account);
+        
         if(id) {
-            if(id === this.body.id && psword === this.body.psword){
+            if(kakao_account.nickname === this.body.kakao_account ){
                 return {success: true, msg:"로그인 성공"};
             }
             return {success: false, msg:"비밀번호가 틀렸습니다"};
         }
         return {success: false, msg: "존재하지 않는 아이디입니다."};
+    } catch (err) {
+        return {success: false, msg: err};
     } 
+    }
 
     async register() {
         
-            const response = await UserStorage.save(this.body);
-            return(response);
+        try{
+        const kakao_account = await UserStorage.getUserInfo(this.body.kakao_account);
+        const response = await UserStorage.save(this.body);
+        if(kakao_account) {
+            if(kakao_account.nickname === this.body.kakao_account){
+                return {success: false, msg:"이미 회원가입이 되어있습니다."};
+            }
+            return{response, msg:"회원가입 완료. "}; 
         }
+        } catch (err) {
+            return {success: false, msg: err};
+            
+        }
+         }
         
     }
 
--- models/UserStorage.js
+++ models/UserStorage.js
@@ -1,26 +1,26 @@
 "use strict";
 
-const { reject } = require("async");
 const db = require("../config/db");
 
 class UserStorage {
 
-     static getUserInfo(id) {
-            return new Promise((resolve, reject) => {
-                const query = "SELECT * FROM Member WHERE MemberName = ?;";
-            db.query(query, [id], (err, data) => {
-                if (err) reject(`${err}`);
+     static getUserInfo(kakao_account) {
+          
+                return new Promise((resolve, reject) => {
+                const query = "SELECT * FROM Member WHERE nickname = ?;";
+                db.query(query, [kakao_account], (err, data) => {
+                if (err) reject(err);
                 resolve(data[0]);   
-            });
-            });
+             });
+             });
         }
 
         static async save(userInfo){
             return new Promise((resolve, reject) => {
-                const query = "INSERT INTO Member(MemberName, MemberPW) VALUES(?, ?);";
+                const query = "INSERT INTO Member(nickname, age_range, birthday, email) VALUES(?, ?, ?, ?);";
             db.query(
                 query,
-                [userInfo.id, userInfo.psword],
+                [userInfo.nickname, userInfo.age_range, userInfo.birth, userInfo.email],
                 (err) => {
                 if (err) reject(`${err}`);
                 resolve({ success: true });
